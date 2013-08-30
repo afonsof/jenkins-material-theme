9593aad5095785b7ef14997aa57b7e66fbfbd6f4
davi4797 <david.porter@rackspace.com>
2013-08-30 18:18:59 -0500

    Add style.css
    Add app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+var script = document.createElement('script');
+script.src = 'http://code.jquery.com/jquery-1.10.1.min.js';
+script.type = 'text/javascript';
+document.getElementsByTagName('head')[0].appendChild(script);
+
+window.onload = function() {
+	jQuery.noConflict();
+	jQuery('#right-top-nav').append(jQuery('#login-field').html());
+	jQuery('body table').last().addClass('footer-table');
+	jQuery('#main-table').wrap('<div id="main-table-wrap" />');
+	jQuery('#main-table').removeAttr('style');
+	jQuery('#main-table tbody').first().prepend('<tr><td colspan="2" id="rackspace-logo"></td></tr>');
+	jQuery('body').show();
+};
+
