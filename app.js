e688107d63ddf7d573fd8ad30a1374e1c3287441
davi4797 <david.porter@rackspace.com>
2013-09-06 10:41:00 -0500

    Begin prototype implementation

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
-var script = document.createElement('script');
-script.src = 'http://code.jquery.com/jquery-1.10.1.min.js';
-script.type = 'text/javascript';
-document.getElementsByTagName('head')[0].appendChild(script);
-
 window.onload = function() {
-	jQuery.noConflict();
-	jQuery('#right-top-nav').append(jQuery('#login-field').html());
-	jQuery('body table').last().addClass('footer-table');
-	jQuery('#main-table').wrap('<div id="main-table-wrap" />');
-	jQuery('#main-table').removeAttr('style');
-	jQuery('#main-table tbody').first().prepend('<tr><td colspan="2" id="rackspace-logo"></td></tr>');
-	jQuery('.behavior-loading').hide();
-	jQuery('body').show();
+//	jQuery('body table').last().addClass('footer-table');
+//	jQuery('#main-table').wrap('<div id="main-table-wrap" />');
+//	jQuery('#main-table').removeAttr('style');
+//	jQuery('#main-table tbody').first().prepend('<tr><td colspan="2" id="rackspace-logo"></td></tr>');
+//	jQuery('.behavior-loading').hide();
+	$('right-top-nav').insert($('login-field').innerHTML);
+//	tables = $$('body').childElements.grep(new Selector('table'));
+//	lastTable = tables[tables.length-1].wrap('div', { 'class': 'main-table-wrap' });
+	$$('body')[0].setStyle({
+	  display: 'block'
+	});
 };
 
