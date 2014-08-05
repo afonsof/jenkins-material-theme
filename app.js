47bc27ac6065f8a95a4788cf3d7823e96ba6e508
davi4797 <david.porter@rackspace.com>
2014-08-04 23:51:59 -0500

    feat(all): update to support ui updates introduced beginning jenkins 1.572 (http://jenkins-ci.org/changelog)
    * Reduce Javascript for minimal modification to new UI
    * Reduce CSS for minimal modification to new UI
    * Introduce tagline-less logo
    * Add Vagrantfile for quick jenkins server build
    * Update before/after images

--- app.js
+++ app.js
@@ -1,38 +1,17 @@
 /**
  * Canon for Jenkins Javascript Overrides
  *
- * Leverages Jankins-included Prototype.js
+ * Leverages Jenkins-included Prototype.js
  *
  * @author Rackspace Web Team
- * @version 0.1
  */
 document.observe("dom:loaded", function () {
-    // Copy auto-refresh link into footer
-    $('footer').insert($('right-top-nav').innerHTML);
-    // Replace auto-refresh link in breadcrumb with login links
-    $('right-top-nav').update($('login-field').innerHTML);
-    // Gather tables
-    var tables = $$('body table');
-    // Add footer-table class to last (footer) table
-    tables[tables.length - 1].toggleClassName('footer-table');
-    // Wrap main table with wrapper div to provide textures background between header and footer
-    $('main-table').wrap('div', { 'id': 'main-table-wrap' });
-    // Clear style attribute for main table
-    $('main-table').setStyle({background:"none"});
-    // Create table row with Rackspace logo and search field cells
-    var logoRow = new Element('tr').update('<td colspan="2" id="rackspace-logo"></td><td id="search-wrap"></td>');
-    // Prepend logo row to main table body
-    $$('#main-table tbody :first-child')[0].insert({
-        before: logoRow
-    });
-    // Copy search form into search-wrap
-    $('search-wrap').update($$('#top-panel table td:nth-child(2)')[0].innerHTML);
-    // Grab searchbox
-    var searchbox = $$('#search-wrap #searchform #search-box')[0];
-    // Set search field input type to text
-    searchbox.type = 'text';
-    // Clear style attribute for search field
-    searchbox.style = null;
+    // Auto Refresh
+    var autoRefreshSelector = $$('#right-top-nav #right-top-nav div.smallfont');
+    if (autoRefreshSelector.length > 0) {
+        var autoRefreshLink = new Element('span').update(autoRefreshSelector[0].innerHTML);
+        $$('span.jenkins_ver')[0].insert({before:autoRefreshLink});
+    }
     // Click logo, go home.
-    $('rackspace-logo').on('click', function(){ location = '/'; });
+    $$('div.logo')[0].on('click', function(){ location = '/'; });
 });
