df3ee71115b4816f9f3015d3d5af134ecd3e8d5e
Afonso F <afonso.franca@gmail.com>
2016-02-01 14:41:02 -0200

    publish script

---
+++ publish.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -e
+
+git checkout master
+FOLDER=$TMPDIR$RANDOM
+VERSION=$(cat package.json | sed -n "s/ *\"version\": *\"\(.*\)\",/\1/p")
+echo Publishing version $VERSION
+
+grunt
+mv dist $FOLDER
+
+git checkout gh-pages
+rm -rf dist
+mv $FOLDER ./dist
+
+read -p "Are you sure? " -n 1 -r
+echo
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    exit 1
+fi
+
+git add dist/
+git commit -m "version $VERSION"
+git push origin gh-pages
